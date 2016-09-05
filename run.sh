315b1198fc84eaaea9ebcbc1b3eb79b4e4e85178
Olivier WATTÉ <olivier@ipeos.com>
2016-09-05 11:00:28 -0400

    add run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,9 @@
 
 sudo docker rm -f `sudo docker ps -aq -f name=php7-dev*`
 
-. docker-compose.env && envsubst < docker-compose.tpl > docker-compose.yml && sudo docker-compose up
+. docker-compose.env && \
+sudo mkdir -p ${MYSQL_VOLUME} && \
+sudo mkdir -p ${APACHE_VOLUME} && \
+envsubst < docker-compose.tpl > docker-compose.yml && \\
+docker-compose up && \
+sudo chown -Rf `whoami`:root ${APACHE_VOLUME}/* 
